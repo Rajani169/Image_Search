250c232552f1ffcb102d35788218716731bd047c
Rajani Sharma <rantasukriti@gmail.com>
2019-08-11 12:38:47 +0530

    Updates

---
+++ precache-manifest.4edeaa2dca9136b45eb70ab3ea9a7beb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52aa7cbf065ac72b664abdf3e5c156b7",
+    "url": "/Image_Search/index.html"
+  },
+  {
+    "revision": "24c6755708ad32be21f5",
+    "url": "/Image_Search/static/css/main.6855211d.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/Image_Search/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "24c6755708ad32be21f5",
+    "url": "/Image_Search/static/js/main.183a5e4d.chunk.js"
+  },
+  {
+    "revision": "730cbc02f36aef3b41ec",
+    "url": "/Image_Search/static/js/runtime~main.4bb41d26.js"
+  }
+]);
